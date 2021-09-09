c3bff8255a79576a7f8562e1a93b0a2ee2d53980
Isaiah Odhner <isaiahodhner@gmail.com>
2021-09-08 23:54:41 -0400

    Fix line intersection!

--- main.js
+++ main.js
@@ -833,6 +833,7 @@ function step() {
 	}
 }
 function intersectLineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
+	x1 += 0.00001; // fix for straight up/down lines (i.e. points falling straight down)
 	var x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
 	var y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
 	if (isNaN(x) || isNaN(y)) {
