4cfadbad96ebd6b5d1c5ff64033b411776b3e681
Isaiah Odhner <isaiahodhner@gmail.com>
2021-09-09 19:47:49 -0400

    Replace line intersection algorithm?

--- main.js
+++ main.js
@@ -900,39 +900,83 @@ function step() {
 		}
 	}
 }
-function intersectLineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
-	x1 += 0.00001; // fix for straight up/down lines (i.e. points falling straight down)
-	x3 -= 0.00001; // fix in case straight up/down line is the second line passed
-	y4 += 0.00001; // might help too, idk (TODO: think about what the degenerate cases are, maybe write tests, maybe find a better algorithm)
-	var x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-	var y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
-	if (isNaN(x) || isNaN(y)) {
-		return false;
-		// console.log(x, y);
-	} else {
-		if (x1 > x2) {
-			if (!(x2 < x && x < x1)) { return false; }
-		} else {
-			if (!(x1 < x && x < x2)) { return false; }
-		}
-		if (y1 >= y2) {
-			if (!(y2 < y && y < y1)) { return false; }
-		} else {
-			if (!(y1 < y && y < y2)) { return false; }
-		}
-		if (x3 >= x4) {
-			if (!(x4 < x && x < x3)) { return false; }
-		} else {
-			if (!(x3 < x && x < x4)) { return false; }
-		}
-		if (y3 >= y4) {
-			if (!(y4 < y && y < y3)) { return false; }
-		} else {
-			if (!(y3 < y && y < y4)) { return false; }
-		}
+// function intersectLineLine(x1, y1, x2, y2, x3, y3, x4, y4) {
+// 	x1 += 0.00001; // fix for straight up/down lines (i.e. points falling straight down)
+// 	x3 -= 0.00001; // fix in case straight up/down line is the second line passed
+// 	y4 += 0.00001; // might help too, idk (TODO: think about what the degenerate cases are, maybe write tests, maybe find a better algorithm)
+// 	var x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+// 	var y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+// 	if (isNaN(x) || isNaN(y)) {
+// 		return false;
+// 		// console.log(x, y);
+// 	} else {
+// 		if (x1 > x2) {
+// 			if (!(x2 < x && x < x1)) { return false; }
+// 		} else {
+// 			if (!(x1 < x && x < x2)) { return false; }
+// 		}
+// 		if (y1 >= y2) {
+// 			if (!(y2 < y && y < y1)) { return false; }
+// 		} else {
+// 			if (!(y1 < y && y < y2)) { return false; }
+// 		}
+// 		if (x3 >= x4) {
+// 			if (!(x4 < x && x < x3)) { return false; }
+// 		} else {
+// 			if (!(x3 < x && x < x4)) { return false; }
+// 		}
+// 		if (y3 >= y4) {
+// 			if (!(y4 < y && y < y3)) { return false; }
+// 		} else {
+// 			if (!(y3 < y && y < y4)) { return false; }
+// 		}
+// 	}
+// 	return { x: x, y: y };
+// }
+function intersectLineLine(line1StartX, line1StartY, line1EndX, line1EndY, line2StartX, line2StartY, line2EndX, line2EndY) {
+    // if the lines intersect, the result contains the x and y of the intersection (treating the lines as infinite) and booleans for whether line segment 1 or line segment 2 contain the point
+    var denominator, a, b, numerator1, numerator2, result = {
+        x: null,
+        y: null,
+        onLine1: false,
+		onLine2: false,
+    };
+    denominator = ((line2EndY - line2StartY) * (line1EndX - line1StartX)) - ((line2EndX - line2StartX) * (line1EndY - line1StartY));
+    if (denominator == 0) {
+        // return result;
+		return;
+    }
+    a = line1StartY - line2StartY;
+    b = line1StartX - line2StartX;
+    numerator1 = ((line2EndX - line2StartX) * a) - ((line2EndY - line2StartY) * b);
+    numerator2 = ((line1EndX - line1StartX) * a) - ((line1EndY - line1StartY) * b);
+    a = numerator1 / denominator;
+    b = numerator2 / denominator;
+
+    // if we cast these lines infinitely in both directions, they intersect here:
+    result.x = line1StartX + (a * (line1EndX - line1StartX));
+    result.y = line1StartY + (a * (line1EndY - line1StartY));
+/*
+        // it is worth noting that this should be the same as:
+        x = line2StartX + (b * (line2EndX - line2StartX));
+        y = line2StartX + (b * (line2EndY - line2StartY));
+        */
+    // if line1 is a segment and line2 is infinite, they intersect if:
+    if (a > 0 && a < 1) {
+        result.onLine1 = true;
+    }
+    // if line2 is a segment and line1 is infinite, they intersect if:
+    if (b > 0 && b < 1) {
+        result.onLine2 = true;
+    }
+    // if line1 and line2 are segments, they intersect if both of the above are true
+	if (result.onLine1 && result.onLine2) {
+		return result;
 	}
-	return { x: x, y: y };
-}
+    // return result;
+};
+
+
 function pointInPolygon(x, y, polygon_points, debug_ctx) {
 	var inside = false;
 	for (var i = 0, j = polygon_points.length - 1; i < polygon_points.length; j = i++) {
