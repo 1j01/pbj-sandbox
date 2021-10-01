3df1d591e25960cfe2cfb32dfcd862de9193478e
Isaiah Odhner <isaiahodhner@gmail.com>
2021-09-30 22:48:15 -0400

    Add fullscreen icon

--- main.js
+++ main.js
@@ -1798,7 +1798,12 @@ function toggleOptionsWindow() {
 			</label>
 		</div>
 		<button id='fullscreen-button' title='Make the application fill the entire screen. Useful especially for mobile, where screens are smaller and browser address bars can cause problems due to their scroll-to-hide feature.'>
-			<span>Fullscreen</span>
+			<span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488.4 488.4" style="height: 1.2em; vertical-align: middle; margin-right: 5px;">
+					<path d="M441 408 339 306l-33 33 102 102h-80v47h160V328h-47zM339 183 441 81v79h47V0H328v47h80L306 150zM150 306 47 408v-80H0v160h160v-47H81l102-102zM160 47V0H0v160h47V81l103 102 33-33L81 47z" fill="currentColor"/>
+				</svg>
+				Fullscreen
+			</span>
 		</button>
 		<h3>Audio:</h3>
 		<label title='Enables or disables sound generation. Note that you need connections between points for sound to work.'>
